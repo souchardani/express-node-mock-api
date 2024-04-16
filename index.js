9a88634e11c0d6d092054032bfe912490f46556d
souchardani <danielsou9782@gmail.com>
2024-04-16 11:59:00 +0200

    changes

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-const express = require('express');
-const { resolve } = require('path');
-
+const express = require("express");
 const app = express();
-const port = 3010;
+const PORT = 8080;
 
-app.use(express.static('static'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get("/tshirt", (req, res) => {
+  res.status(200).send({
+    tshirt: "white",
+    size: "large",
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get("/items", (req, res) => {
+  res.status(200).send({
+    items: [
+      {
+        id: 1,
+        checked: false,
+        item: "Almonds, Unsalted, in the blue bag",
+      },
+      {
+        id: 2,
+        checked: false,
+        item: "Pizza",
+      },
+      {
+        id: 3,
+        checked: false,
+        item: "Bread",
+      },
+    ],
+  });
 });
+
+app.post("/tshirt/:id", (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: "We need a logo" });
+  }
+
+  res.send({
+    tshirt: `tshirt with your ${logo} and id of ${id}`,
+  });
+});
+
+app.listen(PORT, () => console.log(`its alive on http://localhost:${PORT}`));
